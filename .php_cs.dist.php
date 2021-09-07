6c53d57b648afe566304753fc65d5f325fa7baef
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2021-09-07 10:01:05 +0200

    Update .php_cs.dist.php

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -16,7 +16,7 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return PhpCsFixer\Config::create()
     ->setRules([
-        '@PSR2' => true,
+        '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sortAlgorithm' => 'alpha'],
         'no_unused_imports' => true,
