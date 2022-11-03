e10a55d261875fa80e3cf54793004bcb92f956cb
Alex Manase <malexandru699@gmail.com>
2022-11-03 16:42:42 +0200

    refactor to Higher Order Test

--- tests/SectionTest.php
+++ tests/SectionTest.php
@@ -8,55 +8,53 @@ beforeEach(function () {
     $this->navigation = new Navigation(new ActiveUrlChecker('/', '/'));
 });
 
-it('has a title', function () {
-    $section = new Section($this->navigation, 'Hello, world!');
+it('has a title')
+    ->tap(fn () => $this->section = new Section($this->navigation, 'Hello, world!'))
+    ->expect(fn () => $this->section->title)
+    ->toEqual('Hello, world!');
 
-    expect($section->title)->toEqual('Hello, world!');
-});
+it('has an url')
+    ->tap(fn () => $this->section = new Section($this->navigation, 'Hello, world!', '/'))
+    ->expect(fn () => $this->section->url)
+    ->toEqual('/');
 
-it('has an url', function () {
-    $section = new Section($this->navigation, 'Hello, world!', '/');
+it('can have additional attributes')
+    ->tap(
+        fn () =>
+        $this->section = (new Section($this->navigation, 'Hello, world!', '/'))
+            ->attributes(['foo' => 'bar'])
+    )
+    ->expect(fn () => $this->section->attributes)->toHaveKey('foo')
+    ->and(fn () => $this->section->attributes['foo'])->toEqual('bar');
 
-    expect($section->url)->toEqual('/');
-});
+it('can have children')
+    ->tap(fn () => $this->section = (new Section($this->navigation, 'Hello, world!', '/'))->add('Blog', '/posts'))
+    ->expect(fn () => $this->section->children)->toHaveCount(1)
+    ->and(fn () => $this->section->children[0]->title)->toEqual('Blog')
+    ->and(fn () => $this->section->children[0]->url)->toEqual('/posts');
 
-it('can have additional attributes', function () {
-    $section = (new Section($this->navigation, 'Hello, world!', '/'))
-        ->attributes(['foo' => 'bar']);
+it('can configure children')
+    ->tap(
+        fn () => $this->section = (new Section($this->navigation, 'Hello, world!', '/'))
+            ->add('Blog', '/posts', fn (Section $section) => $section->attributes(['baz' => 'qux']))
+    )
+    ->expect(fn () => $this->section->children)->toHaveCount(1)
+    ->and(fn () => $this->section->children[0]->attributes)->toHaveCount(1)
+    ->and(fn () => $this->section->children[0]->attributes)->toHaveKey('baz')
+    ->and(fn () => $this->section->children[0]->attributes['baz'])->toEqual('qux');
 
-    expect($section->attributes)->toHaveKey('foo')
-        ->and($section->attributes['foo'])->toEqual('bar');
-});
+test('attributes can be configured inline')
+    ->tap(
+        fn () => $this->section = (new Section($this->navigation, 'Top level', '/'))
+            ->add('First link', '/link', attributes: ['icon' => 'mdi:link'])
+    )
+    ->expect(fn () => $this->section->children)->toHaveCount(1)
+    ->and(fn () => $this->section->children[0]->attributes)->toHaveKey('icon');
 
-it('can have children', function () {
-    $section = (new Section($this->navigation, 'Hello, world!', '/'))->add('Blog', '/posts');
-
-    expect($section->children)->toHaveCount(1)
-        ->and($section->children[0]->title)->toEqual('Blog')
-        ->and($section->children[0]->url)->toEqual('/posts');
-});
-
-it('can configure children', function () {
-    $section = (new Section($this->navigation, 'Hello, world!', '/'))
-        ->add('Blog', '/posts', fn (Section $section) => $section->attributes(['baz' => 'qux']));
-
-    expect($section->children)->toHaveCount(1)
-        ->and($section->children[0]->attributes)->toHaveCount(1)
-        ->and($section->children[0]->attributes)->toHaveKey('baz')
-        ->and($section->children[0]->attributes['baz'])->toEqual('qux');
-});
-
-test('attributes can be configured inline', function () {
-    $section = (new Section($this->navigation, 'Top level', '/'))
-        ->add('First link', '/link', attributes: ['icon' => 'mdi:link']);
-
-    expect($section->children)->toHaveCount(1)
-        ->and($section->children[0]->attributes)->toHaveKey('icon');
-});
-
-it('has depth', function () {
-    $section = (new Section($this->navigation, 'Hello, world!', '/'))->add('Blog', '/posts');
-
-    expect($section->getDepth())->toEqual(0)
-        ->and($section->children[0]->getDepth())->toEqual(1);
-});
+it('has depth')
+    ->tap(
+        fn () =>
+        $this->section = (new Section($this->navigation, 'Hello, world!', '/'))->add('Blog', '/posts')
+    )
+    ->expect(fn () => $this->section->getDepth())->toEqual(0)
+    ->and(fn () => $this->section->children[0]->getDepth())->toEqual(1);
