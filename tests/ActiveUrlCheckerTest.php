6244499b041f00f10095d146faeca8984fccd8fc
Alex Manase <malexandru699@gmail.com>
2022-11-03 16:27:58 +0200

    refactor `ActiveUrlCheckerTest`

--- tests/ActiveUrlCheckerTest.php
+++ tests/ActiveUrlCheckerTest.php
@@ -1,31 +1,15 @@
 <?php
 
-namespace Spatie\Navigation\Tests;
-
-use Orchestra\Testbench\TestCase;
 use Spatie\Navigation\Helpers\ActiveUrlChecker;
 
-class ActiveUrlCheckerTest extends TestCase
-{
-    /**
-     * @test
-     * @dataProvider urlsProvider
-     */
-    public function it_can_check_if_a_url_is_active(string $currentUrl, string $urlToCheck, bool $result)
-    {
-        $checker = new ActiveUrlChecker($urlToCheck, '/');
+it('can check if an url is active', function (string $currentUrl, string $urlToCheck, bool $result) {
+    $checker = new ActiveUrlChecker($urlToCheck, '/');
 
-        $this->assertSame($result, $checker->check($currentUrl));
-    }
-
-    public function urlsProvider(): array
-    {
-        return [
-            ['/', '/', true],
-            ['https://example.com/', 'https://another-example.com/', false], // hosts don't match
-            ['/foo', '/foo/bar', true],
-            ['/bar', '/foo/bar', false],
-            ['bar', 'bar', false], // Does not start with '/' which is the root path
-        ];
-    }
-}
+    expect($checker->check($currentUrl))->toBe($result);
+})->with([
+    ['/', '/', true],
+    ['https://example.com/', 'https://another-example.com/', false], // hosts don't match
+    ['/foo', '/foo/bar', true],
+    ['/bar', '/foo/bar', false],
+    ['bar', 'bar', false], // Does not start with '/' which is the root path
+]);
