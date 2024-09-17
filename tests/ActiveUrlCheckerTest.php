901f1ac9a2eb611acad8c8b0e03fb0e2f4e8a213
Alexander Shabunevich <loonar@gmail.com>
2024-09-17 18:14:27 +0300

    refactor: move active url check logic to a method

--- tests/ActiveUrlCheckerTest.php
+++ tests/ActiveUrlCheckerTest.php
@@ -13,3 +13,18 @@ it('can check if an url is active', function (string $currentUrl, string $urlToC
     ['/bar', '/foo/bar', false],
     ['bar', 'bar', false], // Does not start with '/' which is the root path
 ]);
+
+it('can strictly check if an url is active', function (string $currentUrl, string $urlToCheck, bool $result) {
+    $checker = new class($urlToCheck, '/') extends ActiveUrlChecker
+    {
+        protected function isActive(string $matchPath, string $itemPath): bool
+        {
+            return $matchPath === $itemPath;
+        }
+    };
+
+    expect($checker->check($currentUrl))->toBe($result);
+})->with([
+    ['/', '/', true],
+    ['/foo', '/foo/bar', false],
+]);
